1cbd0213561d9c8e4f15e50b6683409c4c20a51a
amulkreya <95345629+amulkreya@users.noreply.github.com>
2026-02-10 17:09:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+
 /* ---------------- LOGIN API ---------------- */
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
