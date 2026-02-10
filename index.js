e0c53931ca6370cdbc3f7d0cf14aeba53eebd1eb
amulkreya <95345629+amulkreya@users.noreply.github.com>
2026-02-10 14:06:46 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("SFDC Temp API is running 🚀");
+});
+
+app.get("/health", (req, res) => {
+  res.json({ status: "UP" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
