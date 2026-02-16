7ed57b221ae17f5b4a8a41f919d923eb5a309bc3
amulkreya <95345629+amulkreya@users.noreply.github.com>
2026-02-16 15:19:21 +0530

    Update programs.js

--- donor-app/public/js/programs.js
+++ donor-app/public/js/programs.js
@@ -1,1 +1,14 @@
 
+fetch('/programs')
+.then(res => res.json())
+.then(data => {
+  let rows = '';
+  data.forEach(p => {
+    rows += `
+      <tr>
+        <td>${p.program_name}</td>
+        <td>${p.description}</td>
+      </tr>`;
+  });
+  document.getElementById('programTable').innerHTML = rows;
+});
