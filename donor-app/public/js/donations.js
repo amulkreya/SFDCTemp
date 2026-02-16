71588a04e5e0dd91965f624f7f56d6199976c0ff
amulkreya <95345629+amulkreya@users.noreply.github.com>
2026-02-16 15:20:03 +0530

    Update donations.js

--- donor-app/public/js/donations.js
+++ donor-app/public/js/donations.js
@@ -1,1 +1,20 @@
+function searchDonor() {
+  let mobile = document.getElementById("mobileSearch").value;
+
+  fetch(`/donors/search?mobile=${mobile}`)
+  .then(res => res.json())
+  .then(data => {
+    if(data.length > 0){
+      let d = data[0];
+      document.getElementById("donorInfo").innerHTML =
+        `<div class="alert alert-success">
+          ${d.first_name} ${d.last_name} <br>
+          Email: ${d.email} <br>
+          Donor ID: ${d.donor_id}
+        </div>`;
+    } else {
+      alert("No donor found");
+    }
+  });
+}
 
