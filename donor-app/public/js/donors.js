0558e79d3251402c96db7256de813f6fe3923751
amulkreya <95345629+amulkreya@users.noreply.github.com>
2026-02-16 15:18:06 +0530

    Update donors.js

--- donor-app/public/js/donors.js
+++ donor-app/public/js/donors.js
@@ -1,1 +1,24 @@
 
+let currentPage = 1;
+
+function loadDonors(page = 1) {
+  fetch(`/donors?page=${page}`)
+  .then(res => res.json())
+  .then(data => {
+    let rows = '';
+    data.forEach(d => {
+      rows += `
+        <tr>
+          <td>${d.donor_id}</td>
+          <td>${d.first_name} ${d.last_name}</td>
+          <td>${d.email}</td>
+          <td>${d.mobile}</td>
+          <td>${d.city}</td>
+          <td>${d.state}</td>
+        </tr>`;
+    });
+    document.getElementById('donorTable').innerHTML = rows;
+  });
+}
+
+loadDonors(currentPage);
